9842d1e84c47d9b0951eaa789d1c777eca372e78
burrabobby88-blip <burrabobby88@gmail.com>
2025-11-20 20:54:31 +0530

    Add files via upload

--- binary_search.c
+++ binary_search.c
@@ -14,6 +14,16 @@ scanf("%d",&x);
 low=0;
 high=n-1;
 mid=(low+high)/2;
+while (low<high)
+{
+	if(x==a[mid])
+break;
+else if(x>a[mid])
+low=mid+1;
+else
+high=mid-1;
+mid=(low+high)/2;
+}
 if(x==a[mid])
 printf("search element is found");
 else
