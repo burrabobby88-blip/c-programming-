1a84dfcf683b00df03e09d7e53b6498493944744
burrabobby88-blip <burrabobby88@gmail.com>
2025-12-09 19:34:00 +0530

    Add files via upload

---
+++ DISPLAY ARRAY VALUES USING POINTERS.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int a[5]={10,20,30,40,50};
+	int *p,i;
+	p=a;
+	printf(" Display aray values");
+	for(i=0;i<5;i++)
+	{
+		printf(" %d",*p);
+		p++;
+	}
+	return 0;
+}
